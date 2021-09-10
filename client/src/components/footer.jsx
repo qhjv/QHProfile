1224d2613e1c3c7d2690635048ec07f74a6992b5
Quang Huy <73355744+qhjv@users.noreply.github.com>
2021-09-11 03:22:38 +0700

    gsap text head footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,25 +1,38 @@
-import React from 'react';
+import React,{useRef,useEffect} from 'react';
+import $ from 'jquery'
+import {TimelineLite ,TweenMax, Power3} from 'gsap';
 
 Footer.propTypes = {
     
 };
 
 function Footer(props) {
+    let footer = useRef(null)
+    let link1 = useRef(null)
+    let link2 = useRef(null)
+    let link3 = useRef(null)
+
+    useEffect(() => {
+        TweenMax.to(footer,0,{visibility:'visible'})
+        TweenMax.staggerFrom([link1,link2,link3],1,{opacity:0,x:-20,ease:Power3.easeOut},.8)
+
+    }, [])
+
     return (
-        <footer className="footer">
+        <footer className="footer" ref={el => footer = el}>
                 <div className="d-flex align-items-end justify-content-between">
                     <div className="footer-socials d-flex align-items-center flex-column">
-                        <div className="footer-socials__facebook">
+                        <div className="footer-socials__facebook" ref={el => link1 = el}>
                             <a className="hover-cursor" href="#top">
                                 <i className="fab fa-facebook-square"></i>
                             </a>
                         </div>
-                        <div className="footer-socials__instagram">
+                        <div className="footer-socials__instagram" ref={el => link2 = el}>
                             <a className="hover-cursor" href="#top">
                                 <i className="fab fa-instagram-square"></i>
                             </a>
                         </div>
-                        <div className="footer-socials__github">
+                        <div className="footer-socials__github" ref={el => link3 = el}>
                             <a className="hover-cursor" href="#top">
                                 <i className="fab fa-github-square"></i>
                             </a>
