ab282b4c3993871262ec56d61e04671535501c44
Quang Huy <73355744+qhjv@users.noreply.github.com>
2021-09-12 01:17:54 +0700

    menu blur

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,26 @@ import Noise from './components/noise';
 import Home from './features/home/home';
 import Cursor from './components/cursor';
 import Scrollbar from 'smooth-scrollbar';
+import luxy from 'luxy.js'
 import $ from 'jquery'
+import Menu from './components/menu';
 function App() {
   window.addEventListener('load',() => {
-      Scrollbar.init(document.querySelector('#my-scroll'), {
-          damping:0.01,
-          thumbMinSize:20
+      luxy.init({
+        wrapper: "#luxy",
+        wrapperSpeed: .065
       });
-      Scrollbar.detachStyle()
   })
+  const handleClickBlur = () => {
+        $(".header-logo").removeClass("transform-left");
+        $(".footer-socials").removeClass("transform-left");
+        $("#home").removeClass("transform-left");
+        $(".backgroundBlur").addClass('hidden');
+        $(".menu").removeClass('menutranfrom');
+        $('.spanFast').removeClass('no-trans')
+        $('.spanSlow').removeClass('no-trans')
+        $("body").removeClass('overflow-hidden');
+  }
   // let current = 0,
   //     target = 0, 
   //     ease = 0.01;
@@ -50,10 +61,12 @@ function App() {
         <Noise></Noise>
         <Cursor></Cursor>
         <Header></Header>
-          <div className="wrapper">
-            <div id="my-scroll">
+        <Menu></Menu>
+        <div className="backgroundBlur hidden" 
+              onClick={handleClickBlur}>
+        </div>
+          <div className="wrapper" id="luxy">
               <Home></Home>
-            </div>
           </div>
         <Footer></Footer>
     </div>
